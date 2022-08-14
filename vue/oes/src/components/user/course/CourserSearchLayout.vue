009083c0c3f1ec9cd2b03b5fd77fd9d088579ead
2wangshihan <2766844558@qq.com>
2022-08-14 11:06:51 +0800

    用户静态主页完成

---
+++ vue/oes/src/components/user/course/CourserSearchLayout.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="main">
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "CourserSearchLayout"
+}
+</script>
+
+<style scoped>
+.main{
+  margin-top: 20px;
+}
+</style>
