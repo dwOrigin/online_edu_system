f881d3052afe5e8514ccc46ab773f2d41ba6548f
dw <chen94076@outlook.com>
2022-08-15 16:09:16 +0800

    更新数据库界面

---
+++ springboot/houtai/src/main/java/com/houduan/service/impl/CoursevideoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.houduan.service.impl;
+
+import com.houduan.entity.Coursevideo;
+import com.houduan.mapper.CoursevideoMapper;
+import com.houduan.service.ICoursevideoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author online_system
+ * @since 2022-08-15
+ */
+@Service
+public class CoursevideoServiceImpl extends ServiceImpl<CoursevideoMapper, Coursevideo> implements ICoursevideoService {
+
+}
