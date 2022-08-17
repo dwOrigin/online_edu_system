9047c9d2eb78ce5e0a939ee558c373291db1dbce
文拳 <2362973316@qq.com>
2022-08-17 08:53:25 +0800

    修正部分代码

--- springboot/houtai/src/main/java/com/houduan/service/impl/UserServiceImpl.java
+++ springboot/houtai/src/main/java/com/houduan/service/impl/UserServiceImpl.java
@@ -26,7 +26,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     @Override
     public Result login(String username, String password) {
         QueryWrapper<User> wrapper = new QueryWrapper<>();
-        wrapper.eq("username",username);
+        wrapper.eq("user_name",username);
         if(getOne(wrapper)==null){
             return Result.error(Constants.CODE_400,"用户名不存在");
         } else if (getOne(wrapper).getPassword().equals(password)) {
@@ -38,6 +38,31 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
     @Override
     public Result register(User user) {
-        return null;
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        QueryWrapper<User> wrapper1 = new QueryWrapper<>();
+        QueryWrapper<User> wrapper2 = new QueryWrapper<>();
+        wrapper.eq("user_name",user.getUserName());
+        wrapper1.eq("email",user.getEmail());
+        wrapper2.eq("mobile",user.getMobile());
+        User user1 = getOne(wrapper);
+        User user2 = getOne(wrapper1);
+        if(user1!=null){
+            return Result.error(Constants.CODE_400,"用户名重复");
+        }
+        if(user1!=null){
+            return Result.error(Constants.CODE_400,"邮箱已被注册");
+        }
+        if(user2!=null){
+            return Result.error(Constants.CODE_400,"手机号已被注册");
+        }
+        userMapper.insert(user);
+        return Result.success(Constants.CODE_200,"注册成功");
+    }
+
+    @Override
+    public User searchByName(String userName) {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_name",userName);
+        return getOne(wrapper);
     }
 }
