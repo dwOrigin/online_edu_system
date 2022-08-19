fe585652753c620b114df7adb0113fd5871448e6
文拳 <2362973316@qq.com>
2022-08-19 10:44:05 +0800

    teacher模块功能完成

--- springboot/houtai/src/main/java/com/houduan/service/impl/UserloginlogServiceImpl.java
+++ springboot/houtai/src/main/java/com/houduan/service/impl/UserloginlogServiceImpl.java
@@ -1,11 +1,15 @@
 package com.houduan.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.houduan.entity.Questions;
 import com.houduan.entity.Userloginlog;
 import com.houduan.mapper.UserloginlogMapper;
 import com.houduan.service.IUserloginlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,4 +24,11 @@ public class UserloginlogServiceImpl extends ServiceImpl<UserloginlogMapper, Use
 
     @Autowired
     UserloginlogMapper mapper;
+
+    @Override
+    public List<Userloginlog> getInfoById(Integer user_id) {
+        QueryWrapper<Userloginlog> wrapper = new QueryWrapper<>();
+        wrapper.eq("user_id",user_id);
+        return mapper.selectList(wrapper);
+    }
 }
