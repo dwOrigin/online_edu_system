1d762ed8648c5e18084c38adb2ab167417e2bbff
chen94013 <cdw15939707714@outlook.com>
2022-09-11 17:48:52 +0800

    sql内容

---
+++ sql_files/coursefavorite.sql
@@ -0,0 +1,7 @@
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (1, 4, 3, '2022-08-30 17:10:24');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (3, 5, 10, '2022-08-31 10:16:42');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (4, 1, 7, '2022-08-31 16:47:51');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (5, 5, 4, '2022-08-31 17:26:44');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (6, 4, 6, '2022-08-31 23:33:25');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (7, 1, 4, '2022-09-01 09:48:37');
+INSERT INTO `coursefavorite`(`id`, `course_id`, `user_id`, `add_time`) VALUES (9, 1, 6, '2022-09-02 09:15:34');
---
+++ sql_files/msgreceive.sql
@@ -0,0 +1,7 @@
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (4, '2022-09-02 00:21:23', 2, '2022-09-02 00:21:23', '加油', 0, 1, 6);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (18, '2022-09-01 15:14:55', 6, '2022-09-01 15:14:55', '辛苦你了', NULL, 1, 7);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (19, '2022-08-31 23:33:00', 6, '2022-08-31 23:33:00', '加油喔', NULL, 0, 8);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (23, '2022-09-02 09:42:25', 6, '2022-09-02 09:42:25', '加油！', NULL, 1, 2);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (24, '2022-09-02 09:41:38', 2, '2022-09-02 09:41:38', '这么高级的功能，还不是亮点？', NULL, 0, 6);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (25, '2022-09-02 09:41:55', 2, '2022-09-02 09:41:55', '啊啊啊啊', NULL, 0, 6);
+INSERT INTO `msgreceive`(`id`, `add_time`, `cus_id`, `update_time`, `content`, `type`, `status`, `receiving_cusid`) VALUES (26, '2022-09-02 09:42:13', 2, '2022-09-02 09:42:13', '1111', NULL, 0, 6);
